52c8a7468024954180204455515f4184f6d9e9aa
jinyongp <dev.jinyongp@gmail.com>
2022-02-21 03:53:15 +0900

    Provide api key for github pages

--- public/js/api.example.js
+++
@@ -1,6 +0,0 @@
-/**
- * Fill your api key and rename this file to api.js.
- *
- * @see https://openweathermap.org/api
- */
-const WEATHER_API_KEY = '<Fill Your Weather API Key>';
---
+++ public/js/api.js
@@ -0,0 +1,1 @@
+const WEATHER_API_KEY = '806bdb84f456e6803e40e5efbf6f4e53';
